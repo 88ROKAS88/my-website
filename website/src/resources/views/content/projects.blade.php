95882891c5fb349b51657b14282450d626b69584
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-01 18:17:34 +0200

    project card
    placeholder project card

--- website/src/resources/views/content/projects.blade.php
+++ website/src/resources/views/content/projects.blade.php
@@ -3,4 +3,17 @@
 @section('content')
 
 <h1>CONTENT- projects</h1>
+@foreach ($projects as $project)
+
+
+<div class="card d-flex flex-row">
+    <img src="{{ $project->image }}" class="card-img-top" style="width: 18rem;" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">{{ $project->title }}</h5>
+        <p class="card-text">{{ $project->short_description }}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+
+@endforeach
 @endsection
