fd81e598fac85a1ceb6d02d954990a6d304e7912
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-01 15:35:08 +0200

    small fix
    layout adds correct css

--- website/src/resources/views/layouts/main.blade.php
+++ website/src/resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    @vite(['resources/css/styles.css'])
+    @vite(['resources/css/bootstrap.min.css'])
     <title>{{ config('app.name') }} - @yield('title')</title>
 </head>
 
