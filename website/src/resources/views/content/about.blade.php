202d44b42261141358b8f5862177c22cbf480a4d
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-01 15:30:25 +0200

    placeholder views
    added placeholder views

---
+++ website/src/resources/views/content/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('title','About')
+@section('content')
+
+<h1>CONTENT- about</h1>
+@endsection
---
+++ website/src/resources/views/content/projects.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('title','Projects')
+@section('content')
+
+<h1>CONTENT- projects</h1>
+@endsection
