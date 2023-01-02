9a3bd6a9699d14e6efc59cb2d5432e7b737f5318
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-02 17:07:05 +0200

    fix
    small visual fixes.

--- website/src/resources/views/content/projects.blade.php
+++ website/src/resources/views/content/projects.blade.php
@@ -2,18 +2,17 @@
 @section('title','Projects')
 @section('content')
 
-<h1>CONTENT- projects</h1>
+<p>Here u can find my previos projects and theirs github repositories.</p>
+
 @foreach ($projects as $project)
-
-
-<div class="card d-flex flex-row">
+<div class="card d-flex flex-row my-1">
     <img src="{{ $project->image }}" class="card-img-top" style="width: 18rem;" alt="...">
     <div class="card-body">
         <h5 class="card-title">{{ $project->title }}</h5>
         <p class="card-text">{{ $project->short_description }}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-outline-dark">Read more</a>
     </div>
 </div>
-
 @endforeach
+
 @endsection
