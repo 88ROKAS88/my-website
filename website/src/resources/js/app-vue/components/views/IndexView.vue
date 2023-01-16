b32d23c8f549e2ea4cce586caacb8bc5afc03c65
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-16 19:08:32 +0200

    visual changes
    visual changes on starting page on both website versions.

--- website/src/resources/js/app-vue/components/views/IndexView.vue
+++ website/src/resources/js/app-vue/components/views/IndexView.vue
@@ -3,28 +3,137 @@
     <div class="py-5">
         <h4 class="text-center mt-5">Junior full stack developer</h4>
         <h5 class="text-center">Rokas Misevičius</h5>
-        <h4>My stack:</h4>
-        <ul>
-            <li>
-                Laravel
-                <ul>
-                    <li>Blade</li>
-                </ul>
-            </li>
-            <li>
-                Vue.js
-                <ul>
-                    <li>Vue-router</li>
-                    <li>Pinia</li>
-                </ul>
-            </li>
-            <li>Bootstrap</li>
-            <li>PHP</li>
-            <li>Javascript</li>
-            <li>SQL</li>
-            <li>HTML</li>
-            <li>CSS</li>
-        </ul>
+        <div>
+            <div class="col-md-6 text-center mt-5">
+                <h4>My stack:</h4>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://laravel.com/"
+                                >
+                                    <span class="fw-bold">Laravel</span>
+                                </a>
+                                <br />
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://laravel.com/docs/9.x/blade"
+                                >
+                                    Blade
+                                </a>
+                                <br />
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://laravel.com/docs/9.x/starter-kits#laravel-breeze"
+                                >
+                                    Breeze
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://vuejs.org/"
+                                >
+                                    <span class="fw-bold">Vue.js</span>
+                                </a>
+                                <br />
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://router.vuejs.org/"
+                                >
+                                    Vue-router
+                                </a>
+                                <br />
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://pinia.vuejs.org/"
+                                >
+                                    Pinia
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://getbootstrap.com/"
+                                >
+                                    <span class="fw-bold">Bootstrap</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://www.php.net/"
+                                >
+                                    <span class="fw-bold">PHP</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                                >
+                                    <span class="fw-bold">Javascript</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://en.wikipedia.org/wiki/SQL"
+                                >
+                                    <span class="fw-bold">SQL</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://en.wikipedia.org/wiki/HTML"
+                                >
+                                    <span class="fw-bold">HTML</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a
+                                    target="_blank"
+                                    class="text-black text-decoration-none"
+                                    href="https://en.wikipedia.org/wiki/CSS"
+                                >
+                                    <span class="fw-bold">CSS</span>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+
         <h4>Software I'm using:</h4>
         <ul>
             <li>Docker</li>
--- website/src/resources/views/content/welcome.blade.php
+++ website/src/resources/views/content/welcome.blade.php
@@ -4,11 +4,22 @@
 
 <div class="my-5">
     <h4 class="text-center mt-5">Junior full stack developer</h4>
-    <h5 class="text-center">Rokas</h5>
+    <h5 class="text-center">Rokas Misevičius</h5>
     <h4>My stack:</h4>
     <ul>
-        <li>Laravel</li>
-        <li>Vue.js</li>
+        <li>
+            Laravel
+            <ul>
+                <li>Blade</li>
+            </ul>
+        </li>
+        <li>
+            Vue.js
+            <ul>
+                <li>Vue-router</li>
+                <li>Pinia</li>
+            </ul>
+        </li>
         <li>Bootstrap</li>
         <li>PHP</li>
         <li>Javascript</li>
@@ -24,6 +35,5 @@
         <li>NPM</li>
     </ul>
     <p></p>
-
 </div>
 @endsection
