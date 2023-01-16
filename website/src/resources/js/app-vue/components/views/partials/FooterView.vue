fab6509a46427fcb861d7fc34b72f9e1946f7f00
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-16 19:06:09 +0200

    footer update
    link to cookie policy page from footer.

--- website/src/resources/js/app-vue/components/views/partials/FooterView.vue
+++ website/src/resources/js/app-vue/components/views/partials/FooterView.vue
@@ -1,3 +1,16 @@
 <template>
-    <footer class="p-3 container text-light">Footer placeholder</footer>
+    <footer class="p-3 container text-light">
+        Footer placeholder.
+        <br />
+        The website is under construction.
+        <br />
+        We are using ounly essential cookies. Here u can read our
+        <RouterLink
+            :to="{
+                name: 'cookiepolicy',
+            }"
+        >
+            Cookie Policy </RouterLink
+        >.
+    </footer>
 </template>
--- website/src/resources/views/partials/footer.blade.php
+++ website/src/resources/views/partials/footer.blade.php
@@ -1,3 +1,9 @@
 <footer class="container p-3">
-    Footer placeholder
+    Footer placeholder.
+    <br />
+    The website is under construction.
+    <br />
+    We are using ounly essential cookies. Here u can read our
+
+    <a href="{{ route('cookiepolicy') }}">Cookie Policy</a>.
 </footer>
