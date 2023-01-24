e4a58c82dbe2e9a1ada2b69bddf2c116938d8be7
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-24 11:36:24 +0200

    small changes in footer
    small changes in footer

--- website/src/resources/views/partials/footer.blade.php
+++ website/src/resources/views/partials/footer.blade.php
@@ -1,9 +1,8 @@
 <footer class="container p-3">
-    Footer placeholder.
-    <br />
     The website is under construction.
     <br />
-    We are using ounly essential cookies. Here u can read our
-
+    Visit websites <a href="http://web-dev-rm.com/spa/">SPA version</a>.
+    <br />
+    Website is using ounly essential cookies. Here u can read
     <a href="{{ route('cookiepolicy') }}">Cookie Policy</a>.
 </footer>
