772e5c6e86ca6bbd7a9891bb46098aa01cee2748
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-24 11:22:12 +0200

    fix
    web version - navigation UI fix.

--- website/src/resources/views/partials/header.blade.php
+++ website/src/resources/views/partials/header.blade.php
@@ -2,13 +2,12 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Portfolio Website</a>
+            <a class="navbar-brand" href="{{ route('index') }}">Rokas Misevičius</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link @if(\Request::is('/')) active @endif " aria-current="page" href="{{ route('index') }}">Home</a>
                     <a class="nav-link @if(\Request::is('projects')) active @endif " href="{{ route('projects.index') }}">Projects</a>
                     <a class="nav-link @if(\Request::is('about')) active @endif " href="{{ route('contact') }}">Contact</a>
                 </div>
