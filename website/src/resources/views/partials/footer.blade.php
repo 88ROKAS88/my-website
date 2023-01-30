66b37ff0396de59752ff87914bebe1055ef46411
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-01-30 13:27:29 +0200

    mailer send driver, small changes
    mailer send driver.
    footer visual changes.
    / route set as home page.

--- website/src/resources/views/partials/footer.blade.php
+++ website/src/resources/views/partials/footer.blade.php
@@ -5,6 +5,15 @@
     <br />
     Website is using only essential cookies. Here u can read
     <a href="{{ route('cookiepolicy') }}">Cookie Policy</a>.
+    @guest
     <br />
     <a class="text-body" href="{{ route('login') }}">Log in</a>
+    @endguest
+    @auth
+    <br />
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" class="btn btn-link text-body">Log out</button>
+    </form>
+    @endauth
 </footer>
